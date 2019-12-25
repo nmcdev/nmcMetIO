0210ecafa1eab1e28f569d4145ef54e432f5bb79
NMC-DAVE <daikan1998@163.com>
2019-12-25 17:18:17 +0800

    Add get_map_data functions.

--- R/get_map_data.R
+++ R/get_map_data.R
@@ -1,3 +1,49 @@
+#' Define image size variables from the given bounding box coordinates.
+#'
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param major_dim major image dimension, in pixels. 
+#'                  Default is 400 (meaning larger dimension will be 400 pixels)
+#'
+#' @return list with items "width", "height", and "size" (string of format "<width>,<height>")
+#' @export
+#'
+#' @examples
+#' bbox <- list(
+#'   p1 = list(long = -122.522, lat = 37.707),
+#'   p2 = list(long = -122.354, lat = 37.84)
+#' )
+#' image_size <- define_image_size(bbox, 600)
+#' 
+define_image_size <- function(bbox, major_dim = 400) {
+  require(magrittr)
+  # calculate aspect ration (width/height) from lat/long bounding box
+  aspect_ratio <- abs((bbox$p1$long - bbox$p2$long) / (bbox$p1$lat - bbox$p2$lat))
+  # define dimensions
+  img_width <- ifelse(aspect_ratio > 1, major_dim, major_dim*aspect_ratio) %>% round()
+  img_height <- ifelse(aspect_ratio < 1, major_dim, major_dim/aspect_ratio) %>% round()
+  size_str <- paste(img_width, img_height, sep = ",")
+  list(height = img_height, width = img_width, size = size_str)
+}
+
+
+#' Translate the given long/lat coordinates into an image position (x, y).
+#'
+#' @param long longitude value
+#' @param lat latitude value
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param image_width image width, in pixels
+#' @param image_height image height, in pixels
+#'
+#' @return named list with elements "x" and "y" defining an image position
+#' @export
+#'
+find_image_coordinates <- function(long, lat, bbox, image_width, image_height) {
+  x_img <- round(image_width * (long - min(bbox$p1$long, bbox$p2$long)) / abs(bbox$p1$long - bbox$p2$long))
+  y_img <- round(image_height * (lat - min(bbox$p1$lat, bbox$p2$lat)) / abs(bbox$p1$lat - bbox$p2$lat))
+  list(x = x_img, y = y_img)
+}
+
+
 #' Download a map image from the ArcGIS REST API
 #'
 #' @param bbox bounding box coordinates (list of 2 points with long/lat values)
@@ -15,6 +61,7 @@
 #' API docs: https://developers.arcgis.com/rest/services-reference/export-web-map-task.htm
 #'
 #' @return file path for the downloaded .png map image
+#' @export
 #'
 #' @examples
 #' bbox <- list(
